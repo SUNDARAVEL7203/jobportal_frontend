a3e9881d1ed04ce6149a12d1533425d5584cc2af
SUNDARAVEL7203 <sundarvel.kasi@gmail.com>
2025-04-03 12:37:21 +0530

    url added

--- src/components/utils/constant.js
+++ src/components/utils/constant.js
@@ -1,7 +1,10 @@
 
 const BASE_URL = "https://jobportal-backend-hh41.vercel.app/"
 
-export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
-export const  APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`
+export const USER_API_END_POINT=`${BASE_URL}/api/v1/user`;
+
+export const JOB_API_END_POINT=`${BASE_URL}/api/v1/job`;
+
+export const APPLICATION_API_END_POINT=`${BASE_URL}/api/v1/application`;
+
+export const COMPANY_API_END_POINT=`${BASE_URL}/api/v1/company`;
